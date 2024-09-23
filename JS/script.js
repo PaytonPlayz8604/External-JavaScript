f3e68581e9608e14262ec4487c2414234ae6e48f
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-09-23 08:20:48 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
-alert("I rememebered how to link an external JavaScript file.")
+<script>
+  alert("I rememebered how to link an external JavaScript file.")
+  function buttonalert() {
+      alert("I rememebered how to link an external JavaScript file.")
+  }
+</script>
+
+
